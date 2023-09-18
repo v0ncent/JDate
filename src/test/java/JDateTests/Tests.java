ff57914d6c89c4bfddba72bc2950f178bd96eb76
v0ncent <banksvincent5@gmail.com>
2023-09-18 13:12:13 -0500

    testing around with testing

--- src/test/java/JDateTests/Tests.java
+++ src/test/java/JDateTests/Tests.java
@@ -1,8 +1,16 @@
 package JDateTests;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Tests {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Tests.class);
+    @Test
+    public void startingTests() {
+        System.out.println("Running Tests");
+    }
 
 }
