65518e9336b6f6185dbb01eb9be431c0a8cf3a63
v0ncent <banksvincent5@gmail.com>
2023-09-18 13:15:56 -0500

    got rid of uneeded tests

--- src/test/java/JDateTests/Tests.java
+++ src/test/java/JDateTests/Tests.java
@@ -1,16 +1,8 @@
 package JDateTests;
 
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Tests {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Tests.class);
-    @Test
-    public void startingTests() {
-
-    }
-
+    // no tests yet :(
 }
