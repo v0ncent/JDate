934315554404e4658bbbc700393c54aa283e09a6
v0ncent <83373495+v0ncent@users.noreply.github.com>
2024-01-14 20:59:36 -0600

    cant seem to get equality to work for jframes and jdate

--- src/test/java/JDateTests/Tests.java
+++ src/test/java/JDateTests/Tests.java
@@ -14,28 +14,28 @@ import static org.junit.jupiter.api.Assertions.*;
 public class Tests {
     public static final Logger LOGGER = LoggerFactory.getLogger(Tests.class);
 
-    /**
-     * Test to see the initialization of JDate window.
-     * @implNote  IF THIS TEST IS MAKING IT THROUGH TO YOUR COMMITS IT WILL CAUSE DEV OPS TO FAIL SO COMMENT IT OUT !!!!!!!!!!
-     * @throws InterruptedException if thread is interrupted while sleeping.
-     */
-    // THIS SHOULD BE UNCOMMENTED WHEN PUSHING TO REPOSITORY OTHERWISE DEVOPS WILL FAIL.
-    @Test
-    public void testWindowCreation() throws InterruptedException {
-        final JDate jDateFrame = JDate.getInstance();
-        // sleeps for 5 seconds, so you can check frame but not hold up other tests for long.
-        Thread.sleep(5000);
-        assertNotNull(jDateFrame);
-
-        // ensure that singleton class is working right.
-        final JDate secondObj = JDate.getInstance();
-
-        assertEquals(secondObj, jDateFrame); // Each object is equal to itself
-        // Each object is unequal to null
-        assertNotEquals(jDateFrame, null);
-        // The hashCode of each object in a group is the same as the hash code of each other member of the group
-        assertEquals(jDateFrame.hashCode(), secondObj.hashCode());
-    }
+//    /**
+//     * Test to see the initialization of JDate window.
+//     * @implNote  IF THIS TEST IS MAKING IT THROUGH TO YOUR COMMITS IT WILL CAUSE DEV OPS TO FAIL SO COMMENT IT OUT !!!!!!!!!!
+//     * @throws InterruptedException if thread is interrupted while sleeping.
+//     */
+//    // THIS SHOULD BE UNCOMMENTED WHEN PUSHING TO REPOSITORY OTHERWISE DEVOPS WILL FAIL.
+//    @Test
+//    public void testWindowCreation() throws InterruptedException {
+//        final JDate jDateFrame = JDate.getInstance();
+//        // sleeps for 5 seconds, so you can check frame but not hold up other tests for long.
+//        Thread.sleep(5000);
+//        assertNotNull(jDateFrame);
+//
+//        // ensure that singleton class is working right.
+//        final JDate secondObj = JDate.getInstance();
+//
+//        assertEquals(secondObj, jDateFrame); // Each object is equal to itself
+//        // Each object is unequal to null
+//        assertNotEquals(jDateFrame, null);
+//        // The hashCode of each object in a group is the same as the hash code of each other member of the group
+//        assertEquals(jDateFrame.hashCode(), secondObj.hashCode());
+//    }
 
     /**
      * Test for ensuring the constants are what they need to be.
