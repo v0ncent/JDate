6f597ccdd64472866652e3a751d0bc52e06bf944
v0ncent <83373495+v0ncent@users.noreply.github.com>
2024-07-10 00:35:02 -0500

    We have a window

--- src/main/java/io/github/v0ncent/Main.java
+++ src/main/java/io/github/v0ncent/Main.java
@@ -1,9 +1,24 @@
 package io.github.v0ncent;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.swing.*;
+
 public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    private static final JFrame applicationWindow = new JFrame();
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LOGGER.info("Starting JDate....");
+
+        applicationWindow.setName(Constants.JFrameConstants.WINDOW_NAME);
+        applicationWindow.setTitle(Constants.JFrameConstants.WINDOW_NAME);
+        applicationWindow.setVisible(true);
+        applicationWindow.setSize(Constants.JFrameConstants.WINDOW_SIZE);
+        applicationWindow.setResizable(true);
+        applicationWindow.setDefaultCloseOperation(Constants.JFrameConstants.CLOSE_OPERATION);
     }
 
 }
